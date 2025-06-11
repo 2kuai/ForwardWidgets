27a0e458a0c904cf7bc606d3e514bf582e39d23d
2kuai <88235122+2kuai@users.noreply.github.com>
2025-06-11 22:21:19 +0800

    fix

--- scripts/update-movies-data.js
+++ scripts/update-movies-data.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
 import { promises as fs } from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
